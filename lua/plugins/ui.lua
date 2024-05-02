1df109edda40f8eb158da271b0ce26cabb88487b
Spark <79936503+spa5k@users.noreply.github.com>
2024-05-02 13:11:45 +0000

    Update Neovim configuration and plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -111,5 +111,25 @@ return {
       'nvim-telescope/telescope-fzf-native.nvim'
     }
   },
-
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+  -- {
+  --   "tris203/precognition.nvim",
+  --   config = {
+  -- startVisible = true,
+  -- hints = {
+  --     ["^"] = { text = "^", prio = 1 },
+  --     ["$"] = { text = "$", prio = 1 },
+  --     ["w"] = { text = "w", prio = 10 },
+  --     ["b"] = { text = "b", prio = 10 },
+  --     ["e"] = { text = "e", prio = 10 },
+  -- },
+  -- gutterHints = {
+  --     --prio is not currentlt used for gutter hints
+  --     ["G"] = { text = "G", prio = 1 },
+  --     ["gg"] = { text = "gg", prio = 1 },
+  --     ["{"] = { text = "{", prio = 1 },
+  --     ["}"] = { text = "}", prio = 1 },
+  -- },
+  --   },
+  -- }
 }
